e02be4a72530074da8b765d4e9e43111eac3c97a
mpaz2020 <medpazord2010@gmail.com>
2023-05-24 11:40:59 -0500

    entrega final majo

--- README.md
+++ README.md
@@ -7,9 +7,12 @@
 - database: `control_asistencial`
 - servidor: `MariaDB`
 
-Pasos:
+Pasos: abril el proyecto desde una consola cmd
 
-1. Instalar dependencias con `yarn install`
-2. Restaurar la base de datos: `control_asistencia-202304131747.sql`
-3. Verificar y actualizar el archivo de variables de entorno: `.env`
-4. Correr aplicacion con `yarn start o yarn dev`
+1. Instalar dependencias ejecutar `yarn install`
+2. cd app y ejecutar `yarn install`
+3. luego ejecutar `yarn build`
+4. cd ..
+5. Restaurar la base de datos: `control_asistencia-202304131747.sql`
+6. Verificar y actualizar el archivo de variables de entorno: `.env`
+7. Correr aplicacion ejecutar `yarn start`
--- app/src/boot/auth.js
+++ app/src/boot/auth.js
@@ -4,12 +4,12 @@ import { useAuthStore } from 'stores/auth'
 const whiteList = ['/login', '/auth-redirect']
 
 export default boot(async ({ router }) => {
-  // const authStore = useAuthStore()
-  // await authStore.me()
-  // router.beforeEach((to, from, next) => {
-  //   const isAuthenticated = authStore.isAuthenticated
-  //   if (to.meta.requiresAuth && !isAuthenticated) return next({ path: '/asistencia' })
-  //   if (isAuthenticated && whiteList.includes(to.path)) return next(from)
-  //   return next()
-  // })
+  const authStore = useAuthStore()
+  await authStore.me()
+  router.beforeEach((to, from, next) => {
+    const isAuthenticated = authStore.isAuthenticated
+    if (to.meta.requiresAuth && !isAuthenticated) return next({ path: '/asistencia' })
+    if (isAuthenticated && whiteList.includes(to.path)) return next(from)
+    return next()
+  })
 })
